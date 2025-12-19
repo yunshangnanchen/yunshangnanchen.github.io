e4fad958d80fc51ba233cfee5a9930b0e436d9ab
Nanchen jiajia <130617328+yunshangnanchen@users.noreply.github.com>
2025-12-20 00:38:44 +0800

    部署

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 注意：这里的 base 必须和你的 GitHub 仓库名一致
+  // 如果你的仓库名是 'my-web-app'，则改为 '/my-web-app/'
+  base: '/yunshangnanchen.github.io/',
 })
