5a04737c882a6f8aeb9ad1439ea39dc82cce476d
Nanchen jiajia <130617328+yunshangnanchen@users.noreply.github.com>
2025-12-20 01:18:52 +0800

    名称更改

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // 注意：这里的 base 必须和你的 GitHub 仓库名一致
   // 如果你的仓库名是 'my-web-app'，则改为 '/my-web-app/'
-  base: '/yunshangnanchen.github.io/',
+  base: '/yunshangnanchen/',
 })
